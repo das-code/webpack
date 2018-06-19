3b7ae149edb75c1f193eba8325388182f6679e93
Davi Alves <alves.pindoba@gmail.com>
2018-06-19 10:01:27 -0300

    Add projeto no git

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/principal.js",
+  output: {
+    filename: "principal.js",
+    path: __dirname + "/public"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: []
+      }
+    ]
+  }
+}
