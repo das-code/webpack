382e571cd651a0a6291adb938eb0c22439c20416
Davi Alves <alves.pindoba@gmail.com>
2018-06-20 10:18:44 -0300

    Finalizando esse nodulo do curso

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,43 @@
+const modoDev = process.env.NODE_ENV !== "production"
 const webpack = require("webpack")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const OptimizerCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 module.exports = {
-  mode: "development",
+  mode: modoDev ? "development" : "production",
   entry: "./src/principal.js",
   output: {
     filename: "principal.js",
     path: __dirname + "/public"
   },
+  devServer: {
+    contentBase: "./public",
+    port: 9000
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true
+      }),
+      new OptimizerCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: []
+        test: /\.s?[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }, 
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
       }
     ]
   }
